8bd77f3633a16cf5d7c0e9bb3d7e4eaf4d1683e0
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2022-08-19 12:21:10 +0300

    chore: add some animation

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -3,12 +3,17 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import { auth, db, logout } from "../../firebase/firebase";
 import { query, collection, getDocs, where } from "firebase/firestore";
+import avatar1 from "../login/image/avatar1.png"
+import avatar2 from "../login/image/avatar2.png"
+import avatar3 from "../login/image/avatar3.png"
 import "./home.css"
 
 export const Home : React.FC<any> = () => {
   const [user, loading] = useAuthState(auth);
   const [name, setName] = useState<string>("");
   const navigate = useNavigate();
+  const photo = [avatar1, avatar2, avatar3]
+  const index = Math.floor(Math.random()*photo.length);
   const fetchUserName = async () => {
     try {
       const q = query(collection(db, "users"), where("uid", "==", user?.uid));
@@ -29,7 +34,7 @@ export const Home : React.FC<any> = () => {
   }, [user, loading]);
   return (
     <div className="content">
-        <img src="" alt="" className="picture"/>
+        <img src={photo[index]} alt="" className="picture"/>
        <div>
         Logged in as
          <div>{name}</div>
